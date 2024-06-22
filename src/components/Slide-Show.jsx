358e60ee70a1a17e247d52f2d138672c44a00cc8
Mohammed3Atef <muhammedatef57@gmail.com>
2024-06-22 11:00:47 +0300

    update

--- src/components/Slide-Show.jsx
+++ src/components/Slide-Show.jsx
@@ -58,7 +58,7 @@ export default function CategoriesSlider() {
     slidesToScroll: 1,
     autoplay: true,
     speed: 2000,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 4000,
     cssEase: "linear",
     bool: true,
   };
