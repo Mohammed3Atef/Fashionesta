726f4e01d70f504c3a7dc6698a5ce6ab4fb46d82
Mohammed3Atef <muhammedatef57@gmail.com>
2024-06-22 10:17:32 +0300

    update key error

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,22 +1,52 @@
 export default function Categories() {
   const categories = [
-    "Offers",
-    "Shirt",
-    "T-Shirt",
-    "Sweatpants",
-    "Chino Pants",
-    "Jeans",
-    "Hodi",
-    "Jacket",
-    "Pullover",
+    {
+      CategoryName: "Offers",
+      id: 1,
+    },
+    {
+      CategoryName: "Shirt",
+      id: 2,
+    },
+    {
+      CategoryName: "T-Shirt",
+      id: 3,
+    },
+    {
+      CategoryName: "Sweatpants",
+      id: 4,
+    },
+    {
+      CategoryName: "Chino Pants",
+      id: 5,
+    },
+    {
+      CategoryName: "Jeans",
+      id: 6,
+    },
+    {
+      CategoryName: "Hodi",
+      id: 7,
+    },
+    {
+      CategoryName: "Jacket",
+      id: 8,
+    },
+    {
+      CategoryName: "Pullover",
+      id: 9,
+    },
   ];
   return (
     <div className="shadow-[0px_4px_11px_0px_#00000045] px-[100px] hidden lg:block ">
       <ul className="flex justify-between items-center ">
         {categories.map((category) => {
           return (
-            <li className=" text-xl py-[15px] cursor-pointer transition-[0.3s] hover:border-b hover:border-[black] hover:scale-110">
-              {category}
+            <li
+              key={category.id}
+              className=" text-xl py-[15px] cursor-pointer transition-[0.3s] hover:border-b hover:border-[black] hover:scale-110"
+            >
+              {category.CategoryName}
             </li>
           );
         })}
--- src/components/Slide-Show.jsx
+++ src/components/Slide-Show.jsx
@@ -8,38 +8,47 @@ export default function CategoriesSlider() {
     {
       category: "Sweatpants",
       imageUrl: "https://files.easy-orders.net/1714440092441332325.jpg",
+      id: 1,
     },
     {
       category: "T-Shirt",
       imageUrl: "https://files.easy-orders.net/1712271926203762648.jpg",
+      id: 2,
     },
     {
       category: "Chino Pants",
       imageUrl: "https://files.easy-orders.net/1702498816543921663.jpg",
+      id: 3,
     },
     {
       category: "Shoes",
       imageUrl: "https://files.easy-orders.net/1715795524475163917.png",
+      id: 4,
     },
     {
       category: "Jeans",
       imageUrl: "https://files.easy-orders.net/1714439721304437474.jpg",
+      id: 5,
     },
     {
       category: "Shirt",
       imageUrl: "https://files.easy-orders.net/1716303135641164615.png",
+      id: 6,
     },
     {
       category: "Hodi",
       imageUrl: "https://files.easy-orders.net/1702147201207128236.jpg",
+      id: 7,
     },
     {
       category: "Jacket",
       imageUrl: "https://files.easy-orders.net/1704581391990909111.jpg",
+      id: 8,
     },
     {
       category: "Pullover",
       imageUrl: "https://files.easy-orders.net/1708275507120778767.png",
+      id: 9,
     },
   ];
   const settings = {
@@ -61,7 +70,10 @@ export default function CategoriesSlider() {
       <Slider {...settings}>
         {Categories.map((category) => {
           return (
-            <div className=" text-center  rounded overflow-hidden max-w-[160px]">
+            <div
+              key={category.id}
+              className=" text-center  rounded overflow-hidden max-w-[160px]"
+            >
               <img
                 src={category.imageUrl}
                 alt={category.category}
